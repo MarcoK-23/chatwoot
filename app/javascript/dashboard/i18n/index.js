51599b3657bd9a76ffa962782ff712164f84497f
MarcoK-23 <marcokooistra1@hotmail.com>
2025-07-07 09:39:35 +0200

    Fix missing locale imports in i18n index.js

--- app/javascript/dashboard/i18n/index.js
+++ app/javascript/dashboard/i18n/index.js
@@ -1,4 +1,7 @@
 import ar from './locale/ar';
+import am from './locale/am';
+import az from './locale/az';
+import bg from './locale/bg';
 import ca from './locale/ca';
 import cs from './locale/cs';
 import da from './locale/da';
@@ -11,13 +14,20 @@ import fi from './locale/fi';
 import fr from './locale/fr';
 import he from './locale/he';
 import hi from './locale/hi';
+import hr from './locale/hr';
 import hu from './locale/hu';
+import hy from './locale/hy';
 import id from './locale/id';
+import is from './locale/is';
 import it from './locale/it';
 import ja from './locale/ja';
+import ka from './locale/ka';
 import ko from './locale/ko';
+import lt from './locale/lt';
 import lv from './locale/lv';
 import ml from './locale/ml';
+import ms from './locale/ms';
+import ne from './locale/ne';
 import nl from './locale/nl';
 import no from './locale/no';
 import pl from './locale/pl';
@@ -25,21 +35,29 @@ import pt from './locale/pt';
 import pt_BR from './locale/pt_BR';
 import ro from './locale/ro';
 import ru from './locale/ru';
+import sh from './locale/sh';
 import sk from './locale/sk';
+import sl from './locale/sl';
+import sq from './locale/sq';
 import sr from './locale/sr';
 import sv from './locale/sv';
 import ta from './locale/ta';
 import th from './locale/th';
+import tl from './locale/tl';
 import tr from './locale/tr';
 import uk from './locale/uk';
+import ur from './locale/ur';
+import ur_IN from './locale/ur_IN';
 import vi from './locale/vi';
+import zh from './locale/zh';
 import zh_CN from './locale/zh_CN';
 import zh_TW from './locale/zh_TW';
-import is from './locale/is';
-import lt from './locale/lt';
 
 export default {
   ar,
+  am,
+  az,
+  bg,
   ca,
   cs,
   da,
@@ -52,13 +70,20 @@ export default {
   fr,
   he,
   hi,
+  hr,
   hu,
+  hy,
   id,
+  is,
   it,
   ja,
+  ka,
   ko,
-  ml,
+  lt,
   lv,
+  ml,
+  ms,
+  ne,
   nl,
   no,
   pl,
@@ -66,16 +91,21 @@ export default {
   pt,
   ro,
   ru,
+  sh,
   sk,
+  sl,
+  sq,
   sr,
   sv,
   ta,
   th,
+  tl,
   tr,
   uk,
+  ur,
+  ur_IN,
   vi,
+  zh,
   zh_CN,
   zh_TW,
-  is,
-  lt,
 };
